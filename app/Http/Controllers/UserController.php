10eabbacf859f9a8c3ca36686ce5a30fae9c3d45
Evan Raditya <evanraditya0608@gmail.com>
2026-03-04 05:30:25 +0700

    commit JS4 Praktikum 2.4 – Retreiving or Creating Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,8 +10,17 @@ class UserController extends Controller
 {
     public function index()
     {
-        $jumlah = UserModel::where('level_id', 2)->count();
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama'     => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ]
+        );
 
-        return view('user', ['jumlah' => $jumlah]);
+        $user->save();
+
+        return view('user', ['data' => $user]);
     }
 }
