e0e9ba80e8f96906d7dc148f994bfc7f1ed0e654
Evan Raditya <evanraditya0608@gmail.com>
2026-03-04 00:21:29 +0700

    commit JS4 Praktikum 1 - $fillable:

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,20 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
-        // tambah data user dengan Eloquent Model
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama'     => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
 
-        UserModel::where('username', 'customer-1')->update($data); // update data user
+        UserModel::create($data);
 
-        // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        $user = UserModel::all();
         return view('user', ['data' => $user]);
     }
 }
