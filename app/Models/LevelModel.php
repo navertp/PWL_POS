2a41a285f9ddcb91773eb683e2a40f997f635802
Evan Raditya <evanraditya0608@gmail.com>
2026-03-04 07:31:27 +0700

    commit JS4 Praktikum 2.7 – Relationships

---
+++ app/Models/LevelModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class LevelModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'm_level';
+    protected $primaryKey = 'level_id';
+}
